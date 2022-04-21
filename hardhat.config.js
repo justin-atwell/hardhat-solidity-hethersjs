c5c082d2aac15b5b3656d8b3257fb4b56bcfcc1c
Justin Atwell <justin.atwell@hedera.com>
2022-04-21 10:45:07 -0500

    first Commit

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("hardhat-hethers");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.13",
+  defaultNetwork: 'testnet',
+  hedera: {
+    gasLimit: 300000,
+    networks: {
+      testnet: {
+        accounts: [
+          {
+            "account": '0.0.610551',
+            "privateKey": '<None of your beeswax>'
+          }
+        ]
+      }
+    }
+  }
+};
---
+++ readme.md
@@ -0,0 +1,1 @@
+**JS HARDHAT HEDERA**
